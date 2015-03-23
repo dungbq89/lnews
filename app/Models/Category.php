9c5880667c38938d427259f91758e3583024c385
dungbq89 <dungbq89@gmail.com>
2015-03-23 00:33:46 -0700

    menu top, banner

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,4 +6,14 @@ class Category extends Model {
 
 	//
     protected $table = 'category';
+
+    //get menu
+
+    public static function getMenu($limit)
+    {
+        return Category::where('is_active', '=', 1)
+            ->orderBy('priority', 'desc')
+            ->limit($limit)
+            ->get();
+    }
 }   
--- resources/views/frontend/adv/list.blade.php
+++ resources/views/frontend/adv/list.blade.php
@@ -1,4 +1,4 @@
-<h3>{{ Str::words($boxName, $limit = 5, $end = '...') }}</h3>
+    <h3>{{ Str::words($boxName, $limit = 5, $end = '...') }}</h3>
 {{ $advlist }}
 {{ Str::words('Improving Search on the Envato Marketplaces: 2013 in Review', $limit = 5, $end = '...') }}
 <br />
