ab069ef10c2333029fd537310e2086b6b3cbd072
dinhmv <dinhmai91@gmail.com>
2015-03-29 23:00:39 +0700

    cache

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use App\Models\Article;
 use Illuminate\Contracts\View\Factory;
 use Config;
+use \Cache;
 
 class HomeController extends Controller {
     /*
@@ -29,18 +30,23 @@ class HomeController extends Controller {
     }
 
     public function home() {
-        $category = Category::where('is_active', 1)
-                ->orderBy('created_at')
-                ->take(Config::get('constants.CAT_NUMBER_HOMEPAGE', 4))
-                ->get();
-        $articleByCat = array();
-        foreach ($category as $cat) {
-            $articleByCat[$cat->name_vi] = Article::where('category_id', $cat->id)
-                    ->where('is_active', 2)
-                    ->where('published_time', '<', date('Y-m-d H:i:s', time()))
-                    ->orderBy('published_time')
-                    ->take(9)
+        if (!Cache::has('home_articleByCat')) {
+            $category = Category::where('is_active', 1)
+                    ->orderBy('created_at')
+                    ->take(Config::get('constants.CAT_NUMBER_HOMEPAGE', 4))
                     ->get();
+            $articleByCat = array();
+            foreach ($category as $cat) {
+                $articleByCat[$cat->name_vi] = Article::where('category_id', $cat->id)
+                        ->where('is_active', 2)
+                        ->where('published_time', '<', date('Y-m-d H:i:s', time()))
+                        ->orderBy('published_time')
+                        ->take(9)
+                        ->get();
+            }
+            Cache::put('home_articleByCat', $articleByCat, Config::get('constants.CACHE_TIME', 3));
+        } else {
+            $articleByCat = Cache::get('home_articleByCat', array());
         }
         view()->share('articleByCat', $articleByCat);
         return view('frontend.default.home');
--- config/constants.php
+++ config/constants.php
@@ -2,5 +2,6 @@
 //file : app/config/constants.php
 
 return [
-    'CAT_NUMBER_HOMEPAGE' => 4
+    'CAT_NUMBER_HOMEPAGE' => 4,
+    'CACHE_TIME' => 5
 ];
