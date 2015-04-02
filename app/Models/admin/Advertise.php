da6f9b798991a8ee0bb67ac6be64f12fcb516b24
dungbq89 <dungbq89@gmail.com>
2015-04-02 00:23:19 -0700

    dungba

--- app/Models/admin/Advertise.php
+++ app/Models/admin/Advertise.php
@@ -1,20 +1,36 @@
-<?php namespace App\Models\Admin;
+<?php
+
+namespace App\Models\Admin;
+
 use SleepingOwl\Models\SleepingOwlModel;
-class Advertise extends  SleepingOwlModel {
+use SleepingOwl\Models\Interfaces\ModelWithImageFieldsInterface;
 
-protected $table = 'advertise';
-/**
- * The attributes that are mass assignable.
- *
- * @var array
- */
-protected $fillable = ['name', 'location_type', 'is_active'];
+class Advertise extends SleepingOwlModel implements ModelWithImageFieldsInterface {
 
-/**
- * The attributes excluded from the model's JSON form.
- *
- * @var array
- */
-protected $hidden = [];
+    protected $table = 'advertise';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'image_path', 'location_type', 'link', 'priority', 'is_active'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    public function getImageFields() {
+        return [
+            'image_path' => 'uploads/advertise/'
+        ];
+    }
+
+    public static function boot() {
+        parent::boot();
+    }
 
 }
